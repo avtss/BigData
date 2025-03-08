85a47ac916b7e0cb40fc0adee1a08987801a6be5
avtss <avetiszadikyann@mail.ru>
2025-03-08 21:52:04 +0300

    addition

--- Lab2.1.R
+++ Lab2.1.R
@@ -1,4 +1,5 @@
-#Вывод таблички
+# Вывод таблички
+
 data <- read.csv("result.csv", header = TRUE, sep = ",")
 
 str(data)
@@ -6,7 +7,16 @@ str(data)
 df <- data[,-1]  
 print(df)
 
-#max, min, mean
+#вывод без строк с na:
+df2 <-df[rowSums(is.na(df)) == 0,]
+print(df2)
+
+#замена na на среднее
+library(dplyr)
+df3 <- df %>% na.omit
+print(df3)
+
+# max, min, mean
 stats <- data.frame(
   Max = sapply(df[,-1], max, na.rm = TRUE),
   Min = sapply(df[,-1], min, na.rm = TRUE),
@@ -15,7 +25,7 @@ stats <- data.frame(
 
 print(stats)
 
-#оценки выше 7 и меньше 3
+# оценки выше 7 и меньше 3
 high <- colSums(df[,-1] > 7, na.rm = TRUE)  
 low <- colSums(df[,-1] < 3, na.rm = TRUE)  
 
@@ -23,18 +33,16 @@ preference <- data.frame(Выше_7 = high, Ниже_3 = low)
 
 print(preference)
 
-#сортировка по убыванию
+# сортировка по убыванию
 rating <- sort(colMeans(df[,-1], na.rm = TRUE), decreasing = TRUE)
 
 print(rating)
 
-
-#столбчатая диаграмма оценок
+# Столбчатая диаграмма оценок
 categories <- names(rating) 
 scores <- as.numeric(rating)  
 
-barplot(scores, names.arg = categories, las = 2, col = "lightblue",main = "Рейтинг предпочтений", xlab = "", ylab = "Средний балл", cex.names = 0.5)
-
-
+barplot(scores, names.arg = categories, las = 2, col = "lightblue",
+        main = "Рейтинг предпочтений", xlab = "", ylab = "Средний балл", cex.names = 0.5)
 
 
